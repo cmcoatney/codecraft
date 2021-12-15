c948be80ae77777f2d3be37fbd0e8af12571a05d
Matthew Coatney <mcoatney@bitwiseindustries.com>
2021-12-15 10:08:11 -0800

    chore(api-core): Configure api app to use api-core lib

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,8 +3,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
+import { ApiCoreModule } from '@codecraft/api-core'
+
 @Module({
-  imports: [],
+  imports: [ApiCoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
